6c10e8bdadfe298055da99d62ce93d69553237ff
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-01-11 21:19:13 +0100

    Test folder deleted

--- app/src/main/java/com/example/pietjesbakspel/GameActivity.java
+++ app/src/main/java/com/example/pietjesbakspel/GameActivity.java
@@ -3,12 +3,17 @@ package com.example.pietjesbakspel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        test = findViewById(R.id.test);
     }
 }
--- app/src/main/java/com/example/pietjesbakspel/MainActivity.java
+++ app/src/main/java/com/example/pietjesbakspel/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.pietjesbakspel;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    //Fields that will store button, inputs and errormsg
+    private EditText etPlayer1;
+    private EditText etPlayer2;
+    private Button playButton;
+    private TextView errMsgPlayer1;
+    private TextView errMsgPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Reference to button, edittext and textviews
+        etPlayer1 = (EditText) findViewById(R.id.et_player1);
+        etPlayer2 = (EditText) findViewById(R.id.et_player2);
+
+        playButton = (Button) findViewById(R.id.btn_play);
+
+        errMsgPlayer1 = (TextView) findViewById(R.id.msg_error_pl1);
+        errMsgPlayer2 = (TextView) findViewById(R.id.msg_error_pl2);
+
+        playButton.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String usernamePlayer1 = etPlayer1.getText().toString();
+                String usernamePlayer2 = etPlayer2.getText().toString();
+
+                Intent startGameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+
+                if(usernamePlayer1.trim().equals("") || usernamePlayer2.trim().equals("")){
+                    showErrorMsg(usernamePlayer1,usernamePlayer2);
+                }else{
+                    hideErrorMsg();
+                    startGameActivityIntent.putExtra("pietjesbakSpel.player1", usernamePlayer1);
+                    startGameActivityIntent.putExtra("pietjesbakSpel.player2", usernamePlayer2);
+
+                    startActivity(startGameActivityIntent);
+                }
+            }
+        });
+    }
+
+    private void showErrorMsg(String usernPlayer1, String usernPlayer2){
+        hideErrorMsg();
+
+        if(usernPlayer1.trim().equals("")) errMsgPlayer1.setVisibility(View.VISIBLE);
+
+        if(usernPlayer1.trim().equals("")) errMsgPlayer1.setVisibility(View.VISIBLE);
+    }
+
+    private void hideErrorMsg(){
+        errMsgPlayer1.setVisibility(View.INVISIBLE);
+        errMsgPlayer2.setVisibility(View.INVISIBLE);
     }
 }
